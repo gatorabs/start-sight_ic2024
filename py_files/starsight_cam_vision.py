4db16d6a85a3f9c29d086399550741583bb0032e
Gabriel Spioni <gabrielprivate123@gmail.com>
2024-10-04 00:51:05 -0300

    ADD - Serial Communication
    Implementing Serial Communication With the Microcontroller

--- py_files/starsight_cam_vision.py
+++ py_files/starsight_cam_vision.py
@@ -1,6 +1,12 @@
 import cv2
 import numpy as np
 import time
+import serial
+
+
+serial_port = 'COM5'
+baud_rate = 115200
+ser = serial.Serial(serial_port, baud_rate)
 
 cap = cv2.VideoCapture(0)
 
@@ -19,10 +25,8 @@ while True:
     if time_elap > 0:
         fps = 1.0 / time_elap
 
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.medianBlur(gray, 5)
-
 
     circle = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp=1.2,
                               minDist=30, param1=50, param2=30, minRadius=15,
@@ -30,12 +34,18 @@ while True:
 
     if circle is not None:
         circle = np.round(circle[0, :]).astype("int")
-        x, y, r = circle[0]  # 1 circu
+        x, y, r = circle[0]  # 1 círculo detectado
         cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
         cv2.circle(frame, (x, y), 5, (0, 128, 255), -1)
         cv2.putText(frame, f'FPS: {fps:.2f}', (10, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
         print(f'Círculo detectado: centro= ({x}, {y}), raio= {r}')
+
+
+        ser.write(b'2')
+    else:
+
+        ser.write(b'1')
 
     cv2.imshow("Esferas Real Time", frame)
 
@@ -44,3 +54,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+ser.close()
